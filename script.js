1fca74b3d0b4154e776d94d968849d252d758726
AdriALV2005 <adrimon7221@gmail.com>
2024-03-11 14:00:07 -0500

    cambios sliders

--- script.js
+++ script.js
@@ -45,3 +45,29 @@
     });
   });
   
+
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const slides = document.querySelectorAll('.slide3'); // Cambiado el selector para el nuevo slider
+    let currentSlide = 0;
+  
+    function showSlide(n) {
+      slides[currentSlide].style.display = 'none';
+      currentSlide = (n + slides.length) % slides.length;
+      slides[currentSlide].style.display = 'block';
+    }
+  
+    document.getElementById('prevBtn3').addEventListener('click', function() { // Cambiado el ID del botón anterior para el nuevo slider
+      showSlide(currentSlide - 1);
+    });
+  
+    document.getElementById('nextBtn3').addEventListener('click', function() { // Cambiado el ID del botón siguiente para el nuevo slider
+      showSlide(currentSlide + 1);
+    });
+  });
+  
+
+
+
+
+  
